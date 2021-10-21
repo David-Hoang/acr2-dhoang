37aa4136ad2e0f3aa5372e0755fe9ac6f1ca8790
David <david.hoang@hotmail.fr>
2021-10-22 09:13:50 +1100

    Update router.js

--- src/router.js
+++ src/router.js
@@ -2,25 +2,25 @@ import { createWebHistory, createRouter } from "vue-router";
 
 const routes =  [
   {
-    path: "/",
-    alias: "/restonotes",
-    name: "restonotes",
-    component: () => import("./components/HomeResto")
-  },
-  {
     path: "/edit",
-    name: "/edit",
+    name: "edit",
     component: () => import("./components/EditResto")
   },
   {
     path: "/listing",
-    name: "/listing",
+    name: "listing",
     component: () => import("./components/ListingResto")
   },
   {
     path: "/add",
     name: "add",
     component: () => import("./components/AddResto")
+  },
+  {
+    path: "/",
+    alias: "/restonotes",
+    name: "restonotes",
+    component: () => import("./components/HomeResto")
   }
 ];
 
